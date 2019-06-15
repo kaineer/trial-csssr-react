48a47e89b84a07366d22a6db3e6010ee1728228b
Sergey Kluchkovsky <kaineer@gmail.com>
2019-06-15 20:30:32 +0500

    :mag: Делает IntervalComponent функциональным
    Нет необходимости создавать класс

--- src/components/interval.js
+++ src/components/interval.js
@@ -3,21 +3,17 @@
 import React, { Component } from 'react';
 import { connect } from '../slomux/connect';
 
-class IntervalComponent extends Component {
-  render() {
-    return (
-      <div>
-        <span>
-          Интервал обновления секундомера: {this.props.currentInterval} сек.
-        </span>
-        <span>
-          <button onClick={() => this.props.changeInterval(-1)}>-</button>
-          <button onClick={() => this.props.changeInterval(1)}>+</button>
-        </span>
-      </div>
-    );
-  }
-}
+const IntervalComponent = ({currentInterval, changeInterval}) => (
+  <div>
+    <span>
+      Интервал обновления секундомера: {currentInterval} сек.
+    </span>
+    <span>
+      <button onClick={() => changeInterval(-1)}>-</button>
+      <button onClick={() => changeInterval(1)}>+</button>
+    </span>
+  </div>
+);
 
 export const Interval = connect(
   state => ({
